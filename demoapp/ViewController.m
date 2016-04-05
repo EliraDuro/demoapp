bbdd3524352be09cf538f58671505cdf9937abd1
EliraDuro <eliraduro@gmail.com>
2016-04-04 22:05:25 -0400

    began adding graph api calls

--- demoapp/ViewController.m
+++ demoapp/ViewController.m
@@ -18,10 +18,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
     // Optional: Place the button in the center of your view.
     loginButton.center = self.view.center;
     [self.view addSubview:loginButton];
+    
+    if ([FBSDKAccessToken currentAccessToken]) {
+        NSMutableDictionary* params = [NSMutableDictionary dictionary];
+        [params setValue:@"id,name,email" forKey:@"fields"];
+        
+        FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc]
+                                      initWithGraphPath:@"/{user-id}/likes"
+                                      parameters:params
+                                      
+                                      HTTPMethod:@"GET"];
+        [request startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection,
+                                              id result,
+                                              NSError *error) {
+            NSLog(@"%@", result);
+            NSLog(@"THIS IS A TEST");
+        }];    }
+    
+  
+
 }
 
 - (void)didReceiveMemoryWarning {

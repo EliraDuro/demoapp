57d4dd0b4dc47729d3fa1150d51c6e032d198469
EliraDuro <eliraduro@gmail.com>
2016-04-06 12:24:16 -0400

    began adding graphapi

--- demoapp/ViewController.m
+++ demoapp/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+//#import "GraphAPIReadViewController.h"
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+//#import "AlertControllerUtility.h"
+//#import "PermissionUtility.h"
 
 @interface ViewController ()
 
@@ -18,35 +24,70 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    NSLog(@"@got here1");
+
     FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
     // Optional: Place the button in the center of your view.
     loginButton.center = self.view.center;
     [self.view addSubview:loginButton];
-    
+    loginButton.readPermissions = @[@"email", @"user_friends"];
+     //NSLog(@"@got here1");
     if ([FBSDKAccessToken currentAccessToken]) {
-        NSMutableDictionary* params = [NSMutableDictionary dictionary];
-        [params setValue:@"id,name,email" forKey:@"fields"];
-        
-        FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc]
-                                      initWithGraphPath:@"/{user-id}/likes"
-                                      parameters:params
-                                      
-                                      HTTPMethod:@"GET"];
-        [request startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection,
-                                              id result,
-                                              NSError *error) {
-            NSLog(@"%@", result);
-            NSLog(@"THIS IS A TEST");
-        }];    }
-    
-  
+        // NSLog(@"@got here");
+        if ([[FBSDKAccessToken currentAccessToken] hasGranted:@"publish_actions"]) {
+            // TODO: publish content.
+           
+        } else {
+            
+            FBSDKLoginManager *loginManager = [[FBSDKLoginManager alloc] init];
+            [loginManager logInWithPublishPermissions:@[@"publish_actions"]
+                                   fromViewController:self
+                                              handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+                                                  //TODO: process error or result.
+                                              }];
+        }
+    }
 
+    
+
+    
+    
+}
+
+#pragma mark - Read Users
+
+- (IBAction)readProfile:(id)sender
+{
+    // See https://developers.facebook.com/docs/graph-api/reference/user/ for details.
+    FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc]
+                                  initWithGraphPath:@"/me"
+                                  parameters:@{ @"fields" : @"id, name" }
+                                  HTTPMethod:@"GET"];
+    [request startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+        [self handleRequestCompletionWithResult:result error:error];
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Helper Method
+
+- (void)handleRequestCompletionWithResult:(id)result error:(NSError *)error
+{
+    NSString *title = nil;
+    NSString *message = nil;
+    if (error) {
+        title = @"Graph Request Fail";
+        message = [NSString stringWithFormat:@"Graph API request failed with error:\n %@", error];
+    } else {
+        title = @"Graph Request Success";
+        message = [NSString stringWithFormat:@"Graph API request success with result:\n %@", result];
+    }
+    //UIAlertController *alertController = [AlertControllerUtility alertControllerWithTitle:title message:message];
+    //[self presentViewController:alertController animated:YES completion:nil];
 }
 
 @end
